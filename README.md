f0aab660bc34a198fb9255d03f93c047ddeb6b5d
jenny <jennyb@mit.edu>
2025-04-28 00:26:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# jen-baek.github.io
+
+My personal website.
---
+++ data.js
@@ -0,0 +1,129 @@
+const data = [
+    // Digital = 0
+    [
+        {
+            path: "gallery/digital/music.png",
+            caption: "NUJABES / J DILLA / MF DOOM",
+            description: "(2023)"
+        },
+        {
+            path: "gallery/digital/docm77.png",
+            caption: "Fanart - docm77",
+            description: "(2023) Hermitcraft fanart"
+        },
+        {
+            path: "gallery/digital/transit.png",
+            caption: "the marvels of civil engineering",
+            description: "(2023) Illustration for a college publication that didn't end up getting printed."
+        },
+        {
+            path: "gallery/digital/dragonborn_paladin.png",
+            caption: "Dragonborn Paladin",
+            description: "(2022)"
+        }
+    ],
+    // Traditional = 1
+    [
+        {
+            path: "gallery/traditional/borderline.png",
+            caption: "LET'S GO! LET'S GO!!",
+            description: "(2024) Mural painted in one of the MIT campus tunnels for the Borderline project. Featured here: http://borderline.mit.edu/murals.html"
+        },
+        {
+            path: "gallery/traditional/govtcenter.png",
+            caption: "Government Center",
+            description: "(2023)"
+        },
+        {
+            path: "gallery/traditional/parkstreet.jpg",
+            caption: "Park Street + Street Poetry",
+            description: "(2023)"
+        },
+        {
+            path: "gallery/traditional/bostoncommon.jpg",
+            caption: "Boston Common",
+            description: "(2023)"
+        }
+    ],
+    // MIT = 2
+    [
+        {
+            path: "gallery/mit/classof25_shirt.png",
+            caption: "COOP Graduating Class Merchandise Design Competition - Winner",
+            description: "(2025) Submission for merch design contest by the Harvard/MIT COOP."
+        },
+        {
+            path: "gallery/mit/CPW25.png",
+            caption: "Campus Preview Weekend 2025",
+            description: "Official art for MIT's Campus Preview Weekend 2025."
+        },
+        {
+            path: "gallery/mit/MIT_Poster_2029.png",
+            caption: "MIT Class of 2029 Welcome Poster",
+            description: "(2025) Poster that was delivered to students who were admitted to the MIT Class of 2029. Font: "
+        },
+        {
+            path: "gallery/mit/solar_eclipse.png",
+            caption: "Solar Eclipse",
+            description: "(2024)"
+        },
+        {
+            path: "gallery/mit/arcana.jpg",
+            caption: "The MIT Major Arcana: The Tower",
+            description: "(2023) Featured on CJ Q.'s blog post: https://mitadmissions.org/blogs/entry/mit-major-arcana/"
+        },
+        {
+            path: "gallery/mit/sailor_tim.png",
+            caption: "I AM THE PRETTY GUARDIAN, WHO FIGHTS FOR TECH AND FOR SCIENCE",
+            description: "(2022) A joke drawing that became a sticker and the official avatar for the MIT Admissions Instagram."
+        }
+    ],
+    // Masterpieces = 3
+    [
+        {
+            path: "gallery/masterpieces/grimothy.png",
+            caption: "grimace from mcdonalds x timothee chalamet crossover when???",
+            description: "(2025)"
+        },
+        {
+            path: "gallery/masterpieces/thanksgiving.png",
+            caption: "thanksgiving",
+            description: "(2024)"
+        },
+        {
+            path: "gallery/masterpieces/johnarchy.png",
+            caption: "johnarchy",
+            description: "(2023)"
+        },
+        {
+            path: "gallery/masterpieces/grillmore_girls.png",
+            caption: "grillmore girls",
+            description: "(2023)"
+        },
+        {
+            path: "gallery/masterpieces/soccer_queen.png",
+            caption: "soccer queen",
+            description: "(2022)"
+        },
+        {
+            path: "gallery/masterpieces/nerd.png",
+            caption: "nerds",
+            description: "(2022)"
+        },
+        {
+            path: "gallery/masterpieces/enemies.png",
+            caption: "enemies",
+            description: "(2022)"
+        },
+        {
+            path: "gallery/masterpieces/kaiju.png",
+            caption: "kaiju",
+            description: "(2022)"
+        },
+        {
+            path: "gallery/masterpieces/wizard.png",
+            caption: "wizard",
+            description: "(2022)"
+        }
+    ]
+];
